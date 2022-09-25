b67dfff704081f28f661be1592dc8fb6f8b2fabe
jacobwendell <jwendell2020@gmail.com>
2022-09-25 17:20:03 -0400

    Pseduo code and first instance of gameplay added
    All cases where player has Rock are added

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,31 @@
+// Holder of choices for the computer to pick from randomly
 const CHOICES = ["Rock", "Paper", "Scissors"];
 
+// Create Function that gets random computer shoice by random numbers 0 to 2
+// Random number then picks out a choice from an array
 function getComputerChoice(){
     let randomNumber = Math.floor(Math.random() * 3);
     return CHOICES[randomNumber];
+}
+
+// playRound function which evaluates the player's choice versus the computer random choice
+function playRound(playerSelection, computerSelection){
+    // Need to take in the players choice and make it all lowercase, same with the computers
+    // Check the values against each other
+    // Rock beats Scissors and loses to paper
+    // Scissors beats paper but loses to rock
+    // Paper beats rock but loses to Scissors
+    // return the string declaring the winner with a statement saying etc beats etc
+    let player = playerSelection.toLowerCase();
+    let computer = computerSelection.toLowerCase();
+    if (player === "rock") {
+        if (computer === "rock") {
+            return "A Draw! Rock ties Rock."
+        } else if (computer === "scissors") {
+            return "Player Wins! Rock beats Scissors."
+        } else {
+            return "Computer Wins! Paper beats Rock."
+        }
+    }
+
 }
