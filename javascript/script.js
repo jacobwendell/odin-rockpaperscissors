e0d8b5dd13c79ce16812d5e8c0f105a2f6b4e4f5
jacobwendell <jwendell2020@gmail.com>
2022-09-27 19:14:53 -0400

    Background Highlight when picked added

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,15 @@
+const buttonDiv = document.querySelector(".buttons");
+const buttonRock = document.querySelector("#rock");
+const buttonPaper = document.querySelector("#paper");
+const buttonScissors = document.querySelector("#scissors");
+const displayDiv = document.querySelector(".display");
+const scoreBoard = document.querySelector(".scoreboard");
+const startButton = document.querySelector("#start");
+const playAgainButton = document.querySelector('#play-again');
+const computerRock = document.querySelector("#computer-rock");
+const computerPaper = document.querySelector("#computer-paper");
+const computerScissors = document.querySelector("#computer-scissors");
+
 // Holder of choices for the computer to pick from randomly
 const CHOICES = ["rock", "paper", "scissors"];
 let playerScore = 0;
@@ -23,37 +35,73 @@ function computerScoreIncrease() {
     computerScore += 1;
 }
 
+function removeHighlightBackground() {
+    computerRock.classList.remove("highlight-background1");
+    computerPaper.classList.remove("highlight-background1");
+    computerScissors.classList.remove("highlight-background1");
+}
+
+function removePlayerBackground() {
+    buttonScissors.classList.remove("highlight-background1");
+    buttonRock.classList.remove("highlight-background1");
+    buttonPaper.classList.remove("highlight-background1");
+}
+
+function highlightComputerBackground(computerButton) {
+    removeHighlightBackground();
+    computerButton.classList.add("highlight-background1");
+}
+
+function highlightPlayerBackground(playerButton) {
+    removePlayerBackground();
+    playerButton.classList.add("highlight-background1");
+}
+
+
+
 // playRound function which evaluates the player's choice versus the computer random choice
 function playRound(playerSelection, computerSelection){
     let player = playerSelection;
     let computer = computerSelection;
     if (player === "rock") {
+        highlightPlayerBackground(buttonRock);
         if (computer === "rock") {
+            highlightComputerBackground(computerRock);
             return "A Draw! Rock ties Rock.";
         } else if (computer === "scissors") {
+            highlightComputerBackground(computerScissors);
             playerScoreIncrease();
             return "Player Wins! Rock beats Scissors.";
         } else {
+            highlightComputerBackground(computerPaper);
             computerScoreIncrease();
             return "Computer Wins! Paper beats Rock.";
         }
     } else if (player === "scissors") {
+        highlightPlayerBackground(buttonScissors);
         if (computer === "scissors") {
+            highlightComputerBackground(computerScissors);
             return "A Draw! Scissors ties Scissors.";
         } else if (computer === "paper") {
+            highlightComputerBackground(computerPaper);
             playerScoreIncrease();
             return "Player Wins! Scissors beats Paper";
         } else {
+            highlightComputerBackground(computerRock);
             computerScoreIncrease();
             return "Computer Wins! Rock beats Scissors.";
         }
     } else if (player === "paper"){
+        highlightPlayerBackground(buttonPaper);
         if (computer === "paper") {
+            highlightComputerBackground(computerPaper);
             return "A Draw! Paper ties Paper.";
         } else if (computer === "rock") {
+            highlightComputerBackground(computerRock);
             playerScoreIncrease();
             return "Player Wins! Paper beats Rock"; 
         } else {
+            highlightComputerBackground(computerScissors);
             computerScoreIncrease();
             return "Computer Wins! Scissors beats Paper."
         }
@@ -65,15 +113,6 @@ function playRound(playerSelection, computerSelection){
 }
 
 // GUI 
-const buttonDiv = document.querySelector(".buttons");
-const buttonRock = document.querySelector("#rock");
-const buttonPaper = document.querySelector("#paper");
-const buttonScissors = document.querySelector("#scissors");
-const displayDiv = document.querySelector(".display");
-const scoreBoard = document.querySelector(".scoreboard");
-const startButton = document.querySelector("#start");
-const playAgainButton = document.querySelector('#play-again');
-
 function displayContent(content) {
     displayDiv.textContent = "";
     displayDiv.textContent = content;
@@ -97,6 +136,8 @@ playAgainButton.addEventListener("click", function() {
     computerScore = 0;
     displayContent("Lets Play Rock paper Scissors");
     updateScoreboard();
+    removeHighlightBackground();
+    removePlayerBackground();
     playAgainButton.classList.toggle("hidden");
 })
 
